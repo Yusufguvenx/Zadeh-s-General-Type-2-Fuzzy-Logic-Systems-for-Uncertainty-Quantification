39cded07492cdaaa4e501d376d0f34974637df14
Yusufguvenx <90503042+Yusufguvenx@users.noreply.github.com>
2024-07-08 16:37:00 +0300

    Update GT2_SQR.m

--- Z-GT2-FLS-SQR-ASQR/GT2_SQR.m
+++ Z-GT2-FLS-SQR-ASQR/GT2_SQR.m
@@ -52,7 +52,7 @@ number_mf = 10; % number of rules == number of membership functions
 number_inputs = min(size(x));
 number_outputs = min(size(y));
 
-number_of_epoch = 1;
+number_of_epoch = 10000;
 
 input_membership_type = "gaussmf";
 
